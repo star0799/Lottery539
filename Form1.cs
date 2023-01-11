1c2ec09d261039602af6ec296215b5917850c493
star <superstar0799@gmail.com>
2023-01-11 17:55:38 +0800

    顯示表單

--- Form1.cs
+++ Form1.cs
@@ -14,6 +14,7 @@ namespace Lottery539
     public partial class Form1 : Form
     {
         log log = new log();
+        Helpers helpers = new Helpers();
         public Form1()
         {
             InitializeComponent();
@@ -39,19 +40,33 @@ namespace Lottery539
         }
 
         private void btnQuery_Click(object sender, EventArgs e)
+        {           
+            LoadDataToListView();                   
+        }
+        private void LoadDataToListView()
         {
+            log.WriteLog("匯入資料開始...");
             ReadFile readFile = new ReadFile();
             List<LotteryData> datas = readFile.ReadTxtFile();
-            GetGroupNum(57,datas);            
+            log.WriteLog("匯入資料完成...");
+            Dictionary<string, int> groupNum57 = GetGroupNum(57, datas);
+            Dictionary<string, int> groupNum48 = GetGroupNum(48, datas);
+            List<string> hotNums =groupNum57.Where(g => g.Value > 10).Select(s => s.Key).ToList();
+            string horNumString = helpers.GetNumString(hotNums);
+            foreach (var d in datas)
+            {
+                string hotPointNumString = GetHotPointNum(hotNums,d.Numbers);               
+                lvShow.Items.Add(new ListViewItem(new string[] { d.Issue,d.LotteryDate,"","",d.Numbers, horNumString, hotPointNumString,"" }));
+            }         
+            var a = "";
         }
-        private void GetGroupNum(int groupCount, List<LotteryData> datas)
+        private Dictionary<string, int> GetGroupNum(int groupCount, List<LotteryData> datas)
         {
             List<int> intlist = new List<int>();
             Dictionary<string, int> keyValues = new Dictionary<string, int>();
             string tmpString = string.Empty;
 
             datas = datas.Take(groupCount).ToList();
-
             foreach (var d in datas)
             tmpString += d.Numbers + ",";
             tmpString = tmpString.Substring(0, tmpString.Length - 1);
@@ -78,7 +93,53 @@ namespace Lottery539
                 keyValues.Add(key, x.Value);
             }
             Dictionary<string, int> result = keyValues.OrderBy(o =>Convert.ToInt32(o.Key)).ToDictionary(o => o.Key, p => p.Value);
-            //aaa
+            return result;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ReloadListView();
+        }
+        private void ReloadListView()
+        {
+            lvShow.Clear();
+            lvStatistics.Clear();
+            lvShow.View = View.Details;
+            lvShow.GridLines = true;
+            lvShow.FullRowSelect = true;
+            lvShow.Columns.Add("期號", 80);
+            lvShow.Columns.Add("日期", 150);
+            lvShow.Columns.Add("48期", 80);
+            lvShow.Columns.Add("57期", 80);
+            lvShow.Columns.Add("開獎號碼", 200);
+            lvShow.Columns.Add("熱門號", 200);
+            lvShow.Columns.Add("當期開出熱門號", 150);
+            lvShow.Columns.Add("均值", 70);
+
+            lvStatistics.View = View.Details;
+            lvStatistics.GridLines = true;
+            lvStatistics.FullRowSelect = true;
+            lvStatistics.Columns.Add("期號", 80);
+            lvStatistics.Columns.Add("日期", 80);
+            lvStatistics.Columns.Add("48期", 80);
+            lvStatistics.Columns.Add("57期", 80);
+            lvStatistics.Columns.Add("開獎號碼", 120);
+            lvStatistics.Columns.Add("熱門號", 120);
+            lvStatistics.Columns.Add("當期開出熱門號", 120);
+            lvStatistics.Columns.Add("均值", 70);
+        }
+        public string GetHotPointNum(List<string> hotNums, string num)
+        {
+            string result = string.Empty;
+            var numList=helpers.GetNumList(num);
+            foreach(var n in numList)
+            {
+                if (hotNums.Contains(n))
+                    result += n + ",";
+            }
+            if(result.Length!=0)
+               result = result.Substring(0, result.Length - 1);
+            return result;
         }
     }
 }
---
+++ Helpers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lottery539
+{
+    class Helpers
+    {
+        public string GetNumString(List<string> nums)
+        {
+            string result = string.Empty;            
+            foreach (var s in nums)
+            {
+                result += s.Trim() + ",";
+            }
+            result = result.Substring(0, result.Length - 1);
+            return result;
+        }
+        public string GetNumString(List<int> nums)
+        {
+            string result = string.Empty;
+            foreach (var s in nums)
+            {
+                result += s.ToString().Trim() + ",";
+            }
+            result = result.Substring(0, result.Length - 1);
+            return result;
+        }
+
+        public List<string> GetNumList(string num)
+        {
+            List<string> result =new  List<string>();
+            result = num.Split(',').ToList();
+            return result;
+        }
+    }
+}
